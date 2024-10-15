139ba15cff74277e420705d4f857364925745ffc
Alex Eski <alexeski@gmail.com>
2024-10-15 09:50:07 -0700

    remove duplicated code in unused

--- src/unused/ncsh_parser.h
+++ src/unused/ncsh_parser.h
@@ -5,8 +5,6 @@
 
 #include "ncsh_args.h"
 
-struct ncsh_Args ncsh_parse(char line[], uint_fast32_t length, struct ncsh_Args args);
-
 struct ncsh_Args ncsh_parse_v2(char line[], uint_fast32_t length, struct ncsh_Args args);
 
 #endif // !ncsh_parser_h
