161521349736ea303d8f590cd0e606de00f9e49c
Alex Eski <alexeski@gmail.com>
2024-10-25 15:58:17 -0700

    add back some z files renamed

---
+++ src/z/z.h
@@ -0,0 +1,15 @@
+#ifndef z_main_h
+#define z_main_h
+
+#include <stdint.h>
+
+#include "z_database.h"
+
+enum z_Database_Result z_start (struct z_Database* database);
+
+struct eskilib_String* z_process (const struct eskilib_String* target, struct z_Database* database);
+
+enum z_Database_Result z_finish (struct z_Database* database);
+
+#endif // !z_main_h
+
