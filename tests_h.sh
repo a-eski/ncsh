3b3bd3e86e87f66ffee99bb5b8f784a4fe31b383
Alex Eski <alexeski@gmail.com>
2024-11-13 11:29:07 -0800

    basic integration tests

---
+++ tests_h.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+gcc -std=c2x -Wall -Wextra -Werror -pedantic-errors -Wformat=2 -fsanitize=address,undefined,leak ./src/eskilib/eskilib_string.c ./src/eskilib/eskilib_test.c ./src/eskilib/eskilib_file.c ./src/ncsh_history.c ./src/tests/ncsh_history_tests.c -o ./bin/ncsh_history_tests &&
+./bin/ncsh_history_tests
+
