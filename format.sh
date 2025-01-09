5107808bf0bdf656fa86de82676c089b389e25f7
Alex Eski <alexeski@gmail.com>
2025-01-09 12:04:51 -0800

    update format.sh

--- format.sh
+++ format.sh
@@ -1,7 +1,4 @@
 #!/bin/env bash
 
-# clang-format -style=file -i src/*.c src/*.h
-# clang-format -style=file -i src/eskilib/*.c src/eskilib/*.h
-# clang-format -style=file -i src/z/*.c src/z/*.h
-
 find . -regex '.*\.\(c\|h\)' -exec clang-format -style=file -i {} \;
+
