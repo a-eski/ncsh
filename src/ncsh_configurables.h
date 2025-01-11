52b42df08818bfa0ddb7cd1a1251b8f6fed4fb81
Alex Eski <alexeski@gmail.com>
2025-01-10 16:11:09 -0800

    fix define guards

--- src/ncsh_configurables.h
+++ src/ncsh_configurables.h
@@ -1,21 +1,23 @@
+// Copyright (c) ncsh by Alex Eski 2025
+
 /* NCSH_DEBUG: enable debug mode for the shell */
+#ifndef NCSH_DEBUG
 // #define NCSH_DEBUG
+#endif // !NCSH_DEBUG
 
 /* NCSH_SHORT_DIRETCTORY: only show up to 2 directories in prompt line (defined) or show current path entirely (not
  * defined) */
-#ifdef NCSH_SHORT_DIRECTORY
-#undef NCSH_SHORT_DIRECTORY
-#endif
+#ifndef NCSH_DEBUG
 #define NCSH_SHORT_DIRECTORY
+#endif // !NCSH_DEBUG
 
 /* NCSH_CLEAR_SCREEN_ON_STARTUP: clear screen on startup (defined) or don't clear screen on startup (not defined) */
-#ifdef NCSH_CLEAR_SCREEN_ON_STARTUP
-#undef NCSH_CLEAR_SCREEN_ON_STARTUP
-#endif // NCSH_CLEAR_SCREEN_ON_STARTUP
+#ifndef NCSH_CLEAR_SCREEN_ON_STARTUP
 // #define NCSH_CLEAR_SCREEN_ON_STARTUP
+#endif // !NCSH_CLEAR_SCREEN_ON_STARTUP
 
 /* NCSH_START_TIME: display the amount of milliseconds it took to startup ncsh */
-#ifdef NCSH_START_TIME
-#undef NCSH_START_TIME
-#endif
-#define NCSH_START_TIME
+#ifndef NCSH_START_TIME
+// #define NCSH_START_TIME
+#endif // !NCSH_START_TIME
+
