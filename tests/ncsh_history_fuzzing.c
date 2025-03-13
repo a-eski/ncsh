a880a35ee67f8ffa110004e72db2a5e5aa2110d8
Alex Eski <alexeski@gmail.com>
2025-03-13 15:58:46 -0700

    try to add back clang CI

--- tests/ncsh_history_fuzzing.c
+++ tests/ncsh_history_fuzzing.c
@@ -9,7 +9,6 @@ int LLVMFuzzerTestOneInput(const uint8_t* Data, size_t Size)
 {
     struct ncsh_History history = {};
     ncsh_history_init(eskilib_String_Empty, &history);
-    assert(Size == strlen((char*)Data) + 1);
     ncsh_history_add((char*)Data, Size, &history);
     ncsh_history_exit(&history);
     return 0;
