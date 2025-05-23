73afb74ccfdd77ef8645f451e5fc2900dcaefde2
Alex Eski <alexeski@gmail.com>
2025-05-23 09:17:33 -0700

    remove scratch file

--- src/scratch.c
+++
@@ -1,6 +0,0 @@
-size_t alias_len = len - i - 1;
-printf("val %s len %zu\n", val + i + 1, alias_len);
-aliases[aliases_count].alias->length = alias_len;
-aliases[aliases_count].alias->value = arena_malloc(arena, alias_len, char);
-memcpy(aliases[aliases_count].alias->value, val + i + 1, alias_len);
-aliases[aliases_count].alias->value[alias_len - 1] = '\0';
